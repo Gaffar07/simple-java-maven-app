ee54166882906379b3426781ac76144164076e8c
Gaffar07 <39920290+Gaffar07@users.noreply.github.com>
2023-12-15 20:32:47 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "bye bye bye bye World !";
+    private static final String MESSAGE = "bye bye gaffar!!! !";
 
     public App() {}
 
